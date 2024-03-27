9e9c95b65e83e40575ef4bd37843319257309db0
Kais Radwan <kais.radwan.personal@gmail.com>
2024-03-27 15:50:40 -0400

    Added init imports

--- scoopika_core/__init__.py
+++ scoopika_core/__init__.py
@@ -1,1 +1,3 @@
-# from scoopika.arguments_selection import ArgumentsSelection
+from .arguments_selection import ArgumentsSelection
+from .tool_selection import ToolSelection
+from .prompts import dynamic_prompt
--- scoopika_core/prompts/__init__.py
+++ scoopika_core/prompts/__init__.py
@@ -3,6 +3,7 @@ from .summarize_user_action import (
     system_prompt as summarize_user_action_prompt,
 )
 from .msg import user_msg, system_msg
+from .dynamic import dynamic_prompt
 
 
-__all__ = ["fill_tool_selection_prompt", "summarize_user_action_prompt", "user_msg", "system_msg"]
+__all__ = ["fill_tool_selection_prompt", "summarize_user_action_prompt", "user_msg", "system_msg", "dynamic_prompt"]
